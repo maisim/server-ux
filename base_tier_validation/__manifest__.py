7888af0b25c10f7559495a090f844168bf7c298b
Lois Rilo <lois.rilo@forgeflow.com>
2024-10-25 10:28:08 +0200

    [FIX] base_tier_validation: amend authors
    I just realized that a new author was added to the module during an
    improvement that merged the functionality of base_tier_validation_waiting.
    This was overlook during my review and not explicitly accepted. There
    have been many contributors adding new functionalities and this one
    was not specially big to be included as author.

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
+# Copyright 2017-24 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Base Tier Validation",
